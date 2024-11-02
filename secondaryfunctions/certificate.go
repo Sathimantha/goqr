e513a00dad21f81f84aa4b695d755536f3a269eb
Sathimantha Malalaskera <sathimantha@gmail.com>
2024-11-03 02:59:07 +0530

    manual cert generator deletes cert if already exists

--- secondaryfunctions/certificate.go
+++ secondaryfunctions/certificate.go
@@ -3,6 +3,7 @@ package secondaryfunctions
 import (
 	"fmt"
 	"log"
+	"os"
 	"path/filepath"
 
 	"github.com/Sathimantha/goqr/certificate" // Adjust this import based on your structure
@@ -15,9 +16,21 @@ func GenerateCertificate(studentName, studentID string) (string, error) {
 	}
 
 	fontPath := "assets/Roboto-Regular.ttf" // Update this to the actual path of your TTF font
+	outputDir := filepath.Join(currentDir, "generated_files")
+	certificatePath := filepath.Join(outputDir, studentID+".pdf") // Assuming you name the file using studentID
+
+	// Check if the certificate already exists and delete it
+	if _, err := os.Stat(certificatePath); err == nil {
+		if err := os.Remove(certificatePath); err != nil {
+			return "", fmt.Errorf("Error deleting existing certificate: %v", err)
+		}
+		log.Printf("Deleted existing certificate: %s\n", certificatePath)
+	} else if !os.IsNotExist(err) {
+		return "", fmt.Errorf("Error checking for existing certificate: %v", err)
+	}
 
 	// Initialize the certificate generator
-	generator := certificate.NewGenerator(currentDir, filepath.Join(currentDir, "generated_files"), fontPath)
+	generator := certificate.NewGenerator(currentDir, outputDir, fontPath)
 
 	// Generate the certificate
 	path, err := generator.GenerateCertificate(studentName, studentID)
